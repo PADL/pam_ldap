a56aec0c18f738e928a335427f2c441cd1abed8e
Luke Howard <lukeh@padl.com>
2003-01-30 10:07:12 +0000

    Happy new year

--- pam_ldap.c
+++ pam_ldap.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1998-2002 Luke Howard.
+ * Copyright (C) 1998-2003 Luke Howard.
  * This file is part of the pam_ldap library.
  * Contributed by Luke Howard, <lukeh@padl.com>, 1998.
  *
