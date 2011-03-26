52bf0047c140e5b385c58d6fd6c4fecb7fc6fb50
Luke Howard <lukeh@padl.com>
2011-03-26 12:23:48 +1100

    patch from Ross Tyler to force prompting for the current password when changing expired passwords

--- pam_ldap.c
+++ pam_ldap.c
@@ -3635,7 +3635,7 @@ pam_sm_chauthtok (pam_handle_t * pamh, int flags, int argc, const char **argv)
       if (rc != PAM_SUCCESS)
 	return rc;
 
-      if (!(session->conf->rootbinddn && getuid () == 0))
+      if (!(session->conf->rootbinddn && getuid () == 0 && !(flags & PAM_CHANGE_EXPIRED_AUTHTOK)))
 	{
 	  /* we are not root, authenticate old password */
 	  if (try_first_pass || use_first_pass)
